97515a153b5643e7261f94cbcb304bf89fa377c1
valtyyy <valentind2006@outlook.fr>
2025-03-20 18:24:22 +0100

    fix (installer) : was bad

--- install.sh
+++ install.sh
@@ -1,17 +1,18 @@
+#!/bin/bash
 
 echo "Installing Plug manager"
 
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
-echo "Installing NerdFont"
+echo "Installing NerdFont (Monaspace)"
+mkdir -p /tmp/nerdfonts
+wget -O /tmp/nerdfonts/Monaspace.tar.xz https://github.com/ryanoasis/nerd-fonts/releases/latest/download/Monaspace.tar.xz
 
-curl -OL https://github.com/ryanoasis/nerd-fonts/releases/latest/download/Monaspace.tar.xz
-mkdir /usr/local/share/fonts/Monaspace/
-mv Monospace.tar.xz /usr/local/share/fonts/Monaspace/
-tar -xf /usr/local/share/fonts/Monaspace/Monospace.tar.xf
-rm /usr/local/share/fonts/Monaspace/Monospace.tar.xf
+mkdir -p ~/.local/share/fonts/Monaspace
+tar -xf /tmp/nerdfonts/Monaspace.tar.xz -C ~/.local/share/fonts/Monaspace
 
-echo "Install config"
+fc-cache -fv
 
+echo "Cloning Neovim config"
 git clone "git@github.com:Valtyyy/Nvim_Config.git" ~/.config/nvim/
